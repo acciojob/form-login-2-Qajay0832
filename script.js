dceb0d2f50d0ccae94c1f0d0455f74636ad5680f
Qajay0832 <123375564+Qajay0832@users.noreply.github.com>
2024-06-01 14:11:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ const firstName=document.getElementById("firstName");
 const lastName=document.getElementById("lastName");
 const phoneNumber=document.getElementById("phoneNumber");
 const emailId=document.getElementById("emailId");
-const submit=document.getElementById("submit")
+const submit=document.getElementById("submitBtn")
 submit.addEventListener("click",func)
 function func(){
+	event.preventDefault()
 	alert("First Name: "+firstName.value+" "+"Last Name: "+lastName.value+" Phone Number: "+phoneNumber.value+" Email Id: "+emailId.value)
 }
